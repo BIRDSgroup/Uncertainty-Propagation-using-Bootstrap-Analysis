a54a4257eec382bb53bd7e2a8744d9928936151c
SUGYANI MAHAPATRA <94941168+MSugyani015@users.noreply.github.com>
2025-11-11 17:47:36 +0530

    Simulations and Validation Analysis of RW dataset

---
+++ Simulations and Validation Analysis/main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#$ -cwd
+#$ -o outputfile.txt
+#$ -e errorfile.txt
+
+
+echo "Simulated Dataset"
+echo "Graphs"
+#python SIM_graphs.py
+echo "Validation"
+python SIM_validation.py
+echo "Replication"
+python SIM_replication.py
+
+echo "Semi-Simulated Dataset"
+for tissue in "Muscle_Skeletal" "Whole_Blood" "Skin_Sun_Exposed_Lower_leg" "Thyroid" "Lung";
+do
+	#mkdir "Semi-Simulated Data/$tissue"
+	for s in 73 237;
+	do
+		echo $tissue $s
+		#time python RW_validation.py $tissue "degree" $s
+		#time python RW_validation.py $tissue "pagerank" $s
+	done
+done
